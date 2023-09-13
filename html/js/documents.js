57c5edc44b06204f1cf4b70128940e693f1b60b7
csae8092 <peter.andorfer@oeaw.ac.at>
2023-09-13 11:45:45 +0200

    link to detail views added

--- html/js/documents.js
+++ html/js/documents.js
@@ -3,21 +3,24 @@ const dataUrl = "/data/documents.json"
 
 function listGoods(value, data, type, params, component) {
     let output = value.map((item) => {
-        var category = item.has_category
-        var hasCategory = category.map((cat) => {
-            return `${cat.value}`
-        }).join(", ");
-        console.log(item["has_category"])
-        return `${item.name} (${hasCategory})`
-    }).join("; ");
+        return `<a href="${item.grocerist_id}.html">${item.name}</a>`
+    }).join(" ");
     return `${output}`
+}
+
+function linkToDetailView(cell) {
+    var row = cell.getRow().getData()
+    var cellData = cell.getData()
+    var groceristId = row.grocerist_id
+    var theLink = `<a target="_blank" href="${groceristId}.html">${cellData.shelfmark}</a>`
+    return theLink
 }
 
 d3.json(dataUrl, function (data) {
     tableData = Object.values(data)
 
     var table = new Tabulator("#example-table", {
-        height: 600,
+        height: 800,
         layout: "fitColumns",
         tooltips:true,
         data: tableData,
@@ -27,13 +30,15 @@ d3.json(dataUrl, function (data) {
         },
         columns: [
             {
-                title: "Shelfmark", field: "shelfmark", headerFilter: "input"
+                title: "Shelfmark", field: "shelfmark", headerFilter: "input", formatter: function (cell) {
+                    return linkToDetailView(cell)
+                }
             },
             {
-                title: "Document Id", field: "grocerist_id", headerFilter: "input"
+                title: "Main Person", field: "main_person", mutator: listGoods, headerFilter: "input", formatter: "html"
             },
             {
-                title: "Goods", field: "goods", mutator: listGoods, headerFilter: "input", formatter: "textarea", tooltip:true
+                title: "Goods", field: "goods", mutator: listGoods, headerFilter: "input", formatter: "html", tooltip: true
             },
         ]
     });
