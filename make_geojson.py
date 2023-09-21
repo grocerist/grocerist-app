4048dd58f86f3871e39a6ffeac993a1656695c30
csae8092 <peter.andorfer@oeaw.ac.at>
2023-09-21 13:04:35 +0200

    added script to convert districts.json into geojson

---
+++ make_geojson.py
@@ -0,0 +1,34 @@
+import os
+import json
+
+with open(
+    os.path.join("html", "json_dumps", "districts.json"), "r", encoding="utf-8"
+) as fp:
+    data = json.load(fp)
+
+
+geo_json = {"type": "FeatureCollection", "features": []}
+
+
+for key, value in data.items():
+    if value["lat"]:
+        gj_feature = {
+            "type": "Feature",
+            "geometry": {"type": "Point"},
+            "properties": {},
+        }
+        gj_feature["geometry"]["coordinates"] = [
+            float(value["long"]),
+            float(value["lat"]),
+        ]
+        gj_feature["properties"]["name"] = value["name"]
+        gj_feature["properties"]["grocerist_id"] = value["grocerist_id"]
+        gj_feature["properties"]["doc_count"] = len(value["documents"])
+        gj_feature["properties"]["person_count"] = len(value["persons"])
+
+        geo_json["features"].append(gj_feature)
+
+with open(
+    os.path.join("html", "json_dumps", "gj_districts.json"), "w", encoding="utf-8"
+) as fp:
+    json.dump(geo_json, fp, ensure_ascii=False)
