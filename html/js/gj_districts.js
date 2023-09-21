bd8797917a4d6dab6f65f91309fa7345c191864b
csae8092 <peter.andorfer@oeaw.ac.at>
2023-09-21 13:18:28 +0200

    minor

--- html/js/gj_districts.js
+++ html/js/gj_districts.js
@@ -20,14 +20,14 @@ d3.json(gjDataUrl, function (data) {
 
     L.geoJSON(data, {
 
-		filter(feature, layer) {
-			if (feature.properties) {
-				// If the property "underConstruction" exists and is true, return false (don't render features under construction)
-				return feature.properties.underConstruction !== undefined ? !feature.properties.underConstruction : true;
-			}
-			return false;
-		},
+        filter(feature, layer) {
+            if (feature.properties) {
+                // If the property "underConstruction" exists and is true, return false (don't render features under construction)
+                return feature.properties.underConstruction !== undefined ? !feature.properties.underConstruction : true;
+            }
+            return false;
+        },
 
-		onEachFeature
-	}).addTo(map);
+        onEachFeature
+    }).addTo(map);
 })
