2322eea45a526a8d4404df6ee8fada4669caf90f
kisram <s2kisram@uni-trier.de>
2023-12-12 12:41:38 +0100

    Made Goods and Documents cells scrollable

--- html/js/categories.js
+++ html/js/categories.js
@@ -1,11 +1,10 @@
-
 const dataUrl = "json_dumps/categories.json"
 
-function listDocs(value, data, type, params, component) {
+function linkList(value, data, type, params, component) {
     let output = value.map((item) => {
         return `<li><a href="${item.grocerist_id}.html">${item.name}</a></li>`
     }).join(" ");
-    return `<ul>${output}</ul>`
+    return `<ul class="list-unstyled">${output}</ul>`
 }
 
 function linkToDetailView(cell) {
@@ -15,14 +14,6 @@ function linkToDetailView(cell) {
     var theLink = `<a href="${groceristId}.html">${cellData.name}</a>`
     return theLink
 }
-
-function mutateDistrictField(value, data, type, params, component) {
-    let output = value.map((item) => {
-        return `<li><a href="${item.grocerist_id}.html">${item.name}</a></li>`
-    }).join(" ");
-    return `<ul class="list-unstyled">${output}</ul>`
-}
-
 
 d3.json(dataUrl, function (data) {
     tableData = Object.values(data)
@@ -44,13 +35,20 @@ d3.json(dataUrl, function (data) {
                 }, bottomCalc: "count"
             },
             {
-                title: "Goods", field: "goods", mutator: mutateDistrictField, linkToDetailView: "input", formatter: "html", headerFilter: "input"
+                title: "Goods", field: "goods", mutator: linkList, headerFilter: "input", 
+                formatter: function (cell) {
+                    return get_scrollable_cell(this, cell);
+                },
             },
             {
                 title: "# Goods", field: "good_count", bottomCalc: "sum"
             },
             {
-                title: "Documents", field: "documents", mutator: listDocs, headerFilter: "input", formatter: "html", tooltip: true
+                title: "Documents", field: "documents", mutator: linkList, headerFilter: "input",
+                formatter: function (cell) {
+                    return get_scrollable_cell(this, cell);
+                },
+                tooltip: true
             },
             {
                 title: "# Docs", field: "doc_count", bottomCalc: "sum"
@@ -58,6 +56,3 @@ d3.json(dataUrl, function (data) {
         ]
     });
 });
-
-
-
