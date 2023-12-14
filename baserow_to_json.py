5b8e8ff65e6633f5bfe6580c8b68e7dad4e38148
kisram <s2kisram@uni-trier.de>
2023-12-14 14:35:13 +0100

    First steps to integrate script form @cfhaak

--- baserow_to_json.py
+++ baserow_to_json.py
@@ -7,49 +7,46 @@ from config import br_client, BASEROW_DB_ID, JSON_FOLDER
 play_id_2_play_name = None
 altname_keys = ["alt_tokens", "legacy"]
 
+
 def make_tabulator_data_entry(
-        name: str,
-        lng: str,
-        lat: str,
-        geonames_url: str,
-        internal_id: str,
-        mentions: list,
-        alt_names: list,
-        total_occurences: int
-    ):
+    name: str,
+    lng: str,
+    lat: str,
+    geonames_url: str,
+    internal_id: str,
+    mentions: list,
+    alt_names: list,
+    total_occurences: int,
+):
     """creates dict for row if name, lng, lat and mentions are True, else None"""
     if name and lng and lat and mentions:
         return {
-            "coordinates": {
-            "lng": lng,
-            "lat": lat
-            },
+            "coordinates": {"lng": lng, "lat": lat},
             "name": name,
-            "geonames": [
-                "geonames",
-                geonames_url
-            ],
+            "geonames": ["geonames", geonames_url],
             "internal_id": internal_id,
             "mentions": mentions if mentions else [],
             "alt_names": alt_names,
-            "total_occurences": total_occurences
+            "total_occurences": total_occurences,
         }
     else:
         return None
 
 
 def create_tabulator_data(
-        json_file_path:str,
-        name_key: str,
-        lng_key: str,
-        lat_key: str,
-        geonames_url_key: str,
-        internal_id_key: str,
-        mentions_key: str,
-        altnames_keys: list,
-        total_occurences_keys: str
-    ):
-    tabulator_data_output_path = f"{json_file_path.removesuffix('.json')}_tabulator.json"
+    json_file_path: str,
+    name_key: str,
+    lng_key: str,
+    lat_key: str,
+    geonames_url_key: str,
+    internal_id_key: str,
+    mentions_key: str,
+    altnames_keys: list,
+    total_occurences_keys: str,
+):
+    tabulator_data_output_path = (
+        f"{json_file_path.removesuffix('.json')}_tabulator.json"
+    )
     tabulator_data = []
     with open(json_file_path, "r") as json_file_io:
         json_data = json.load(json_file_io)
@@ -62,17 +59,19 @@ def create_tabulator_data(
                 geonames_url=row[geonames_url_key],
                 internal_id=row[internal_id_key],
                 mentions=row[mentions_key],
-                alt_names=[row[altnames_key] for altnames_key in altnames_keys if row[altnames_key]],
-                total_occurences=row[total_occurences_keys] if row[total_occurences_keys] else 1
+                alt_names=[
+                    row[altnames_key]
+                    for altnames_key in altnames_keys
+                    if row[altnames_key]
+                ],
+                total_occurences=row[total_occurences_keys]
+                if row[total_occurences_keys]
+                else 1,
             )
             if new_row is not None:
                 tabulator_data.append(new_row)
     with open(tabulator_data_output_path, "w") as tabulator_data_dumpfile:
-        json.dump(
-            tabulator_data,
-            fp=tabulator_data_dumpfile,
-            indent=2
-        )
+        json.dump(tabulator_data, fp=tabulator_data_dumpfile, indent=2)
     return tabulator_data_output_path
 
 
@@ -89,9 +88,9 @@ def lookup_play(play_index):
 
 def delete_rows_in_dump(json_file_path: str, test_2_fieldname: dict):
     """
-    test_2_fieldname: dict, 
+    test_2_fieldname: dict,
         key = fieldname,
-        val = function that takes fieldname 
+        val = function that takes fieldname
         and returns true if it qualifies the row to be deleted
     """
     print(f"deleting rows in {json_file_path}")
@@ -116,14 +115,18 @@ def delete_rows_in_dump(json_file_path: str, test_2_fieldname: dict):
     return json_file_path
 
 
-def modify_fields_in_dump(json_file_path: str, fieldnames_to_manipulations: dict, write:bool=True):
+def modify_fields_in_dump(
+    json_file_path: str, fieldnames_to_manipulations: dict, write: bool = True
+):
     """
     Loads json file from json_file_path and performs a set of manipulations.
-    fieldnames_to_manipulations contains a set of fieldnames-strings as keys, 
+    fieldnames_to_manipulations contains a set of fieldnames-strings as keys,
     defining the set of fields to be manipulated, and as corresponding value
     a function performing the desired manipulations
     """
-    print(f"updating {', '.join(fieldnames_to_manipulations.keys())} in {json_file_path}")
+    print(
+        f"updating {', '.join(fieldnames_to_manipulations.keys())} in {json_file_path}"
+    )
     # load data
     json_data = None
     with open(json_file_path, "r") as json_file_io:
@@ -150,48 +153,32 @@ def get_play_title_for_mentions(mentions: list):
     new_mentions = []
     if len(mentions) != 0:
         for mention in mentions:
-            new_mentions.append(
-                [
-                    lookup_play(mention["id"])["title"],
-                    mention["value"]
-                ]
-            )
+            new_mentions.append([lookup_play(mention["id"])["title"], mention["value"]])
     return new_mentions
 
 
 if __name__ == "__main__":
     os.makedirs(JSON_FOLDER, exist_ok=True)
     json_file_paths = br_client.dump_tables_as_json(
-        BASEROW_DB_ID,
-        folder_name=JSON_FOLDER, 
-        indent=2
+        BASEROW_DB_ID, folder_name=JSON_FOLDER, indent=2
     )
     places_filepath = f"{JSON_FOLDER}/places.json"
     vienna_places_filepath = f"{JSON_FOLDER}/vienna_places.json"
     person_filepath = f"{JSON_FOLDER}/persons.json"
     if os.path.isfile(person_filepath):
-        fieldnames_to_manipulations = {
-            "occurences" : get_play_title_for_mentions
-        }
+        fieldnames_to_manipulations = {"occurences": get_play_title_for_mentions}
         modfied_data = modify_fields_in_dump(
-            person_filepath,
-            fieldnames_to_manipulations,
-            False
+            person_filepath, fieldnames_to_manipulations, False
         )
         with open(person_filepath, "w") as outfile:
-            json.dump(
-                [val for val in modfied_data.values()],
-                outfile,
-                indent=2
-            )
+            json.dump([val for val in modfied_data.values()], outfile, indent=2)
     if os.path.isfile(places_filepath):
         fieldnames_to_manipulations = {
-            "geonames" : get_normalized_uri,
-            "mentioned_in" : get_play_title_for_mentions
+            "geonames": get_normalized_uri,
+            "mentioned_in": get_play_title_for_mentions,
         }
         modfied_file_path = modify_fields_in_dump(
-            places_filepath,
-            fieldnames_to_manipulations
+            places_filepath, fieldnames_to_manipulations
         )
         create_tabulator_data(
             json_file_path=modfied_file_path,
@@ -201,28 +188,19 @@ if __name__ == "__main__":
             geonames_url_key="geonames",
             internal_id_key="nestroy_id",
             mentions_key="mentioned_in",
-            altnames_keys=[
-                "alt_tokens",
-                "legacy"
-            ],
-            total_occurences_keys="total_occurences"
+            altnames_keys=["alt_tokens", "legacy"],
+            total_occurences_keys="total_occurences",
         )
     if os.path.isfile(vienna_places_filepath):
-        test_2_fieldname = {
-            "geonames" : lambda x: not bool(x.strip())
-        }
+        test_2_fieldname = {"geonames": lambda x: not bool(x.strip())}
         fieldnames_to_manipulations = {
-            "geonames" : get_normalized_uri,
-            "mentioned_in" : get_play_title_for_mentions
+            "geonames": get_normalized_uri,
+            "mentioned_in": get_play_title_for_mentions,
         }
         modfied_file_path = modify_fields_in_dump(
-            vienna_places_filepath,
-            fieldnames_to_manipulations
+            vienna_places_filepath, fieldnames_to_manipulations
         )
-        modfied_file_path = delete_rows_in_dump(
-            modfied_file_path,
-            test_2_fieldname
-        )
+        modfied_file_path = delete_rows_in_dump(modfied_file_path, test_2_fieldname)
         create_tabulator_data(
             json_file_path=modfied_file_path,
             name_key="survey_id",
@@ -231,11 +209,8 @@ if __name__ == "__main__":
             geonames_url_key="geonames",
             internal_id_key="nestroy_id",
             mentions_key="mentioned_in",
-            altnames_keys=[
-                "variants",
-                "modern_name"
-            ],
-            total_occurences_keys="total_occurences"
+            altnames_keys=["variants", "modern_name"],
+            total_occurences_keys="total_occurences",
         )
     for path in json_file_paths:
         print(path)
---
+++ html/js/districts_test.js
@@ -0,0 +1,295 @@
+
+let map_cfg = {
+    initial_zoom: "5",
+    initial_coordinates: [48.210033, 16.363449],
+    //base_map_url: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+    base_map_url: "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
+    json_url: "https://github.com/grocerist/grocerist-data/blob/main/json_dumps/districts.json",
+    on_row_click_zoom: "11",
+    div_id: "map",
+    attribution:
+        '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors © <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: "abcd",
+    maxZoom: 20,
+};
+let table_cfg = {
+    maxHeight: "45vh",
+    layout: "fitColumns",
+    width: "100%",
+    headerFilterLiveFilterDelay: 600,
+    responsiveLayout: "collapse",
+    paginationCounter: "rows",
+    pagination: "local",
+    paginationSize: 10,
+    langs: {
+        default: {
+            pagination: {
+                counter: {
+                    showing: "",
+                    of: "of",
+                    rows: "",
+                },
+            },
+        },
+    },
+};
+
+
+function fetch_tabulatordata_and_build_table(map_cfg, map, table_cfg, marker_layer) {
+	console.log("loading table");
+	fetch(map_cfg.json_url)
+		.then(function (response) {
+			// json string
+			return response.json();
+		})
+		.then(function (tabulator_data) {
+			// the table will draw all markers on to the empty map
+			table_cfg.data = tabulator_data;
+			let table = build_map_table(table_cfg);
+			populateMapFromTable(table, map, map_cfg.on_row_click_zoom, marker_layer);
+		})
+		.catch(function (err) {
+			console.log(err);
+		});
+}
+
+function get_html_link(name, url) {
+	return `<a href='${url}'>${name}</a>`;
+}
+
+function get_html_list(array) {
+	return `<ul><li>${array.join("</li><li>")}</li></ul>`;
+}
+
+function get_label_string_html(row, frequency) {
+	let number_of_plays_mentioning = frequency;
+	// could use properties.total_occurences later
+	let label_string = `<b>${row.name}</b><br>(occurring in ${number_of_plays_mentioning} plays)<br>`;
+	let plays_list_start = "<ul>";
+	let plays_list_end = "</ul>";
+	row.mentions.forEach((mention) => {
+		let internal_id = mention[1];
+		let play_title = mention[0];
+		plays_list_start += `<li><a href='${internal_id}'>${play_title}</a></li>`;
+	});
+	return label_string + plays_list_start + plays_list_end;
+}
+
+function draw_cirlce_from_rowdata(latLng, frequency) {
+	let radius = frequency;
+	let html_dot = "";
+	let border_width = 4;
+	let border_color = "red";
+	let size = radius * 10;
+	let circle_style = `style="width: ${size}px; height: ${size}px; border-radius: 50%; display: table-cell; border: ${border_width}px solid ${border_color};  background: rgba(255, 0, 0, .5); overflow: hidden; position: absolute"`;
+	let iconSize = size;
+	let icon = L.divIcon({
+		html: `<span ${circle_style}>${html_dot}</span>`,
+		className: "",
+		iconSize: [iconSize, iconSize],
+	});
+	let marker = L.marker(latLng, {
+		icon: icon,
+	});
+	return marker;
+}
+
+function zoom_to_point_from_row_data(row_data, map, zoom, existing_markers_by_coordinates) {
+	let coordinate_key = get_coordinate_key_from_row_data(row_data);
+	let marker = existing_markers_by_coordinates[coordinate_key];
+	marker.openPopup();
+	L.map.setView([row_data.coordinates.lat, row_data.coordinates.lng], zoom);
+}
+
+function make_cell_scrollable(table, cell, cell_html_string_in) {
+	var cell_html_element = cell.getElement();
+	cell_html_element.style.whiteSpace = "pre-wrap";
+	cell_html_element.style.overflow = "auto";
+	cell_html_element.style.maxHeight = "100px";
+	if (cell_html_string_in !== undefined) {
+		return table.emptyToSpace(cell_html_string_in);
+	} else {
+		return table.emptyToSpace(cell.getValue());
+	}
+}
+
+function build_linklist_cell(table, cell) {
+	let values = cell.getValue();
+	let i = 0;
+	let links = [];
+	while (i < values.length) {
+		let pair = values[i];
+		links.push(get_html_link(pair[0], pair[1]));
+		i++;
+	}
+	let basic_html = get_html_list(links);
+	return make_cell_scrollable(table, cell, basic_html);
+}
+
+function get_coordinate_key_from_row_data(row_data) {
+	return row_data.coordinates.lat + row_data.coordinates.lng;
+}
+
+function init_map_from_rows(rows, marker_layer) {
+	console.log("populating map with icons");
+	let existing_circles_by_coordinates = {};
+	rows.forEach((row) => {
+		let row_data = row.getData();
+		let coordinate_key = get_coordinate_key_from_row_data(row_data);
+		let frequency = row_data.mentions.length;
+		let new_circle = draw_cirlce_from_rowdata(
+			[row_data.coordinates.lat, row_data.coordinates.lng],
+			frequency,
+		);
+		existing_circles_by_coordinates[coordinate_key] = new_circle;
+		new_circle.bindPopup(get_label_string_html(row_data, frequency));
+		new_circle.addTo(marker_layer);
+	});
+	return existing_circles_by_coordinates;
+}
+
+function toggle_marker_visibility(marker) {
+	if (marker._icon.style.display === "table-cell") {
+		marker._icon.style.display = "none";
+	} else if (marker._icon.style.display === "none") {
+		marker._icon.style.display = "table-cell";
+	} else {
+		// after pageload there is no value direct value there
+		// its still table-cell cause css
+		marker._icon.style.display = "none";
+	}
+}
+
+function populateMapFromTable(table, map, on_row_click_zoom, marker_layer) {
+	table.on("tableBuilt", function () {
+		console.log("built table");
+		let all_rows = this.getRows();
+		var existing_markers_by_coordinates = init_map_from_rows(all_rows, marker_layer);
+		// every marker is displayed …
+		var keys_of_displayed_markers = Object.keys(existing_markers_by_coordinates);
+		table.on("dataFiltered", function (filters, rows) {
+			if (rows.length < 4 && rows.length > 0) {
+				let row_data = rows[0].getData();
+				zoom_to_point_from_row_data(
+					row_data,
+					map,
+					on_row_click_zoom,
+					existing_markers_by_coordinates,
+				);
+			} else {
+				map.setView([48.210033, 16.363449], 5);
+			}
+			let keys_of_markers_to_be_displayed = [];
+			rows.forEach((row) => {
+				let row_data = row.getData();
+				let coordinate_key = get_coordinate_key_from_row_data(row_data);
+				keys_of_markers_to_be_displayed.push(coordinate_key);
+			});
+			// hide & display filtered markers
+			Object.entries(existing_markers_by_coordinates).forEach(([coordinate_key, marker]) => {
+				if (keys_of_markers_to_be_displayed.includes(coordinate_key)) {
+					// this marker should be displayed
+					if (!keys_of_displayed_markers.includes(coordinate_key)) {
+						// it is not beeing displayed
+						// display it
+						// marker_layer.addLayer(marker);
+						toggle_marker_visibility(marker);
+						keys_of_displayed_markers.push(coordinate_key);
+						console.log(marker);
+					}
+				} else {
+					// this marker should be hidden
+					if (keys_of_displayed_markers.includes(coordinate_key)) {
+						// it is not hidden
+						// hide it
+						// marker_layer.removeLayer(marker);
+						//console.log(marker);
+						let index_of_key = keys_of_displayed_markers.indexOf(coordinate_key);
+						keys_of_displayed_markers.splice(index_of_key, 1);
+						toggle_marker_visibility(marker);
+					}
+				}
+			});
+		});
+		//eventlistener for click on row
+		table.on("rowClick", function (event, row) {
+			let row_data = row.getData();
+			zoom_to_point_from_row_data(
+				row_data,
+				map,
+				on_row_click_zoom,
+				existing_markers_by_coordinates,
+			);
+		});
+	});
+}
+
+function build_map_table(table_cfg) {
+	if (!("columns" in table_cfg)) {
+		table_cfg.columns = [
+			{
+				title: "Name",
+                field: "name",
+                headerFilter: "input",
+                formatter: function (cell) {
+                    return linkToDetailView(cell)
+                }
+			},
+		];
+	}
+	let table = new Tabulator("#places_table", table_cfg);
+	console.log("made table");
+	return table;
+}
+
+/////////////////////
+// building the map//
+/////////////////////
+function build_map_and_table(map_cfg, table_cfg, wms_cfg = null) {
+	console.log("loading map");
+	let map = L.map(map_cfg.div_id).setView(map_cfg.initial_coordinates, map_cfg.initial_zoom);
+	let tile_layer = L.tileLayer(map_cfg.base_map_url, {
+		maxZoom: map_cfg.max_zoom,
+		attribution: map_cfg.attribution,
+	});
+	let marker_layer = L.layerGroup();
+	// handle the layers
+	// order of adding matters!
+	tile_layer.addTo(map);
+	// this is for the page gui / switch for toggling overlays
+	let overlay_control = {
+		"modern map": tile_layer,
+		"mentioned entities": marker_layer,
+	};
+	// if cfg is provided wms map layer gets added
+	if (wms_cfg !== null) {
+		let wms_layer = L.tileLayer.wms(wms_cfg.wms_url, wms_cfg.wmsOptions);
+		wms_layer.addTo(map);
+		overlay_control["Stadtplan 1858 (k.k. Ministerium des Inneren)"] = wms_layer;
+	}
+	// this has to happen here, in case historical map gets added
+	marker_layer.addTo(map);
+	var layerControl = L.control.layers(null, overlay_control);
+	layerControl.addTo(map);
+	fetch_tabulatordata_and_build_table(map_cfg, map, table_cfg, marker_layer);
+}
+
+function build_table(table_id, table_cfg, json_url) {
+	console.log("loading table");
+	fetch(json_url)
+		.then(function (response) {
+			// json string
+			return response.json();
+		})
+		.then(function (tabulator_data) {
+			// the table will draw all markers on to the empty map
+			table_cfg.data = tabulator_data;
+			let table = new Tabulator(table_id, table_cfg);
+			console.log("made table");
+		})
+		.catch(function (err) {
+			console.log(err);
+		});
+}
+
+build_map_and_table(map_cfg, table_cfg);
