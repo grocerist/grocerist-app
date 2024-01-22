18d753dd4524c9ad4ab29f94483d98b2acafa0b9
kisram <Kinga.Sramo@oeaw.ac.at>
2024-01-22 15:58:34 +0100

    Linting

--- make_chartsjson.py
+++ make_chartsjson.py
@@ -74,15 +74,12 @@ with open(
 ) as file:
     docs_data = json.load(file)
 
-# match year_of_creation__hicri from documents.json to the goods categories
-# for each category, create a dictionary in a dictionary with the key as the decade and the value as the number of documents in that decade
-# format for the time series chart
-# get a list of all the decades
+    # get a list of all the decades
     decades = set()
     for doc in docs_data:
         year_of_creation = docs_data[doc]["year_of_creation__hicri"]
-        if year_of_creation != None:
-            decade = (round(int(year_of_creation),-1))
+        if year_of_creation is not None:
+            decade = round(int(year_of_creation), -1)
             decades.add(decade)
     decades = sorted(decades)
     decade_dict = {}
@@ -92,31 +89,36 @@ with open(
         for decade in decades:
             # initialize for each decade so there will be a value even if there were no documents in that decade
             decade_dict[category_name][decade] = 0
-        
-        doc_list =  []
+
+        doc_list = []
         for document in category["documents"]:
             doc_list.append(document["id"])
         for doc in doc_list:
             year_of_creation = docs_data[doc]["year_of_creation__hicri"]
-            if year_of_creation != None:
-                decade = (round(int(year_of_creation),-1))
+            if year_of_creation is not None:
+                decade = round(int(year_of_creation), -1)
                 decade_dict[category_name][decade] += 1
 
 print(decade_dict)
 
-categories =  [str(x) for x in decades]
-      
+categories = [str(x) for x in decades]
 
-decades_results= []
+
+decades_results = []
 # sort categories so they will be displayed in an alphabetical order later
 sorted_categories = sorted(decade_dict.keys())
 for category in sorted_categories:
     docs_per_decade = list(decade_dict[category].values())
+    # only do this for the first category
+
     decades_results.append(
-            {"name": category, "marker": {
-                "symbol": 'square'
-            }, "data": docs_per_decade}
-        )
+        {
+            "name": category,
+            "marker": {"symbol": "square"},
+            "visible": "false",
+            "data": docs_per_decade,
+        }
+    )
 
 # Convert the results to JSON format and write to a file
 result_json = json.dumps(
@@ -124,7 +126,10 @@ result_json = json.dumps(
         "religions": religions_results,
         "categories": categories_results,
         "categories_drilldown": categories_drilldown,
-        "categories_over_decades": {"categories" : categories, "series": decades_results}
+        "categories_over_decades": {
+            "categories": categories,
+            "series": decades_results,
+        },
     },
     indent=2,
 )
