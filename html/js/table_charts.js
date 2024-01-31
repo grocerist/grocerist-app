9006f93e2093ca17990bc2d69ab6bdf84ee2bd4b
kisram <Kinga.Sramo@oeaw.ac.at>
2024-01-31 13:26:13 +0100

    made religions chart responsive to filtering

--- html/js/table_charts.js
+++ html/js/table_charts.js
@@ -3,7 +3,49 @@ const titleStyle = {
   color: '#79B4A9',
   fontWeight: 'bold',
   fontSize: '20px'
+}
 
+const TABLE_CFG = {
+  pagination: true,
+  paginationSize: 15,
+  height: 800,
+  layout: 'fitColumns',
+  tooltips: true,
+  responsiveLayout: 'collapse',
+  persistence: {
+    headerFilter: true
+  },
+  columns: [
+    {
+      title: 'Name',
+      field: 'name',
+      headerFilter: 'input',
+      formatter: function (cell) {
+        return linkToDetailView(cell)
+      }
+    },
+    {
+      title: 'Religion',
+      field: 'religion',
+      mutator: mutateSelectField,
+      headerFilter: 'input',
+      formatter: 'html'
+    },
+    {
+      title: 'Documents',
+      field: 'documents',
+      mutator: mutateDocumentField,
+      headerFilter: 'input',
+      formatter: 'html'
+    },
+    {
+      title: 'District',
+      field: 'district',
+      mutator: mutateDistrictField,
+      headerFilter: 'input',
+      formatter: 'html'
+    }
+  ]
 }
 function mutateSelectField (value, data, type, params, component) {
   let output = value
@@ -70,84 +112,61 @@ function createPieChart (containerId, title, data) {
     ]
   })
 }
+function calculateReligionData (rows) {
+  let religionCount = {}
+  rows.forEach(row => {
+    let rowData = row.getData()
+    let religionKey = rowData.religion.replace(/(<([^>]+)>)/gi, '')
+    // Update the count for a specific religion
+    religionCount[religionKey] = (religionCount[religionKey] || 0) + 1
+  })
+  // Calculate the total number of persons
+  let totalPersons = Object.values(religionCount).reduce(
+    (acc, count) => acc + count,
+    0
+  )
+
+  // Function to calculate percentage  and round it to 2 decimal places
+  function calculatePercentage (count, total) {
+    let percentage = (count / total) * 100
+    return parseFloat(percentage.toFixed(2))
+  }
+
+  // Calculate the percentages for each religion and store them in an array
+  let religionsResults = Object.entries(religionCount).map(
+    ([religion, count]) => ({
+      name: religion,
+      y: calculatePercentage(count, totalPersons)
+    })
+  )
+  return religionsResults
+}
+
+function createTable (TABLE_CFG) {
+  console.log('loading table')
+  const table = new Tabulator('#persons-table', TABLE_CFG)
+  return table
+}
+
 d3.json(dataUrl, function (data) {
   tableData = Object.values(data)
+  TABLE_CFG.data = tableData
+  const table = createTable(TABLE_CFG)
 
-  var table = new Tabulator('#persons-table', {
-    pagination: true,
-    paginationSize: 15,
-    height: 800,
-    layout: 'fitColumns',
-    tooltips: true,
-    data: tableData,
-    responsiveLayout: 'collapse',
-    persistence: {
-      headerFilter: true
-    },
-    columns: [
-      {
-        title: 'Name',
-        field: 'name',
-        headerFilter: 'input',
-        formatter: function (cell) {
-          return linkToDetailView(cell)
-        }
-      },
-      {
-        title: 'Religion',
-        field: 'religion',
-        mutator: mutateSelectField,
-        headerFilter: 'input',
-        formatter: 'html'
-      },
-      {
-        title: 'Documents',
-        field: 'documents',
-        mutator: mutateDocumentField,
-        headerFilter: 'input',
-        formatter: 'html'
-      },
-      {
-        title: 'District',
-        field: 'district',
-        mutator: mutateDistrictField,
-        headerFilter: 'input',
-        formatter: 'html'
-      }
-    ]
-  })
-  let dummyData = [{ name: 'District 1', y: 57 },{ name: 'District 2', y: 43 }] 
-  
+  let dummyData = [
+    { name: 'District 1', y: 57 },
+    { name: 'District 2', y: 43 }
+  ]
+
   table.on('tableBuilt', function () {
     let rows = table.getRows()
-    let religionCount = {}
-    rows.forEach(row => {
-      let rowData = row.getData()
-      let religionKey = rowData.religion.replace(/(<([^>]+)>)/gi, '')
-      // Update the count for a specific religion
-      religionCount[religionKey] = (religionCount[religionKey] || 0) + 1
-    })
-    // Calculate the total number of persons
-    let totalPersons = Object.values(religionCount).reduce(
-      (acc, count) => acc + count,
-      0
-    )
-
-    // Function to calculate percentage  and round it to 2 decimal places
-    function calculatePercentage (count, total) {
-      let percentage = (count / total) * 100
-      return parseFloat(percentage.toFixed(2))
-    }
-
-    // Calculate the percentages for each religion and store them in an array
-    let religionsResults = Object.entries(religionCount).map(
-      ([religion, count]) => ({
-        name: religion,
-        y: calculatePercentage(count, totalPersons)
-      })
-    )
-
+    let religionsResults = calculateReligionData(rows)
     createPieChart('religion-chart', 'Religion', religionsResults)
     createPieChart('districts-chart', 'Districts', dummyData)
   })
+  table.on('dataFiltered', function (_filters, rows) {
+    let religionsResults = calculateReligionData(rows)
+    createPieChart('religion-chart', 'Religion', religionsResults)
+  })
 })
+
