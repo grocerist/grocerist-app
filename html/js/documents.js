4298af5b8f261465dd825ebe01113789a8e55577
kisram <Kinga.Sramo@oeaw.ac.at>
2024-07-08 16:08:39 +0200

    changed approach based on https://tabulator.info/examples/6.2#menu

--- html/js/documents.js
+++ html/js/documents.js
@@ -3,6 +3,55 @@ const dataUrl = 'json_dumps/documents.json'
 d3.json(dataUrl, function (data) {
   tableData = Object.values(data)
 
+  //define column header menu as column visibility toggle
+  const headerMenu = function () {
+    let menu = []
+    const allColumns = this.getColumns()
+
+    allColumns.forEach(column => {
+      let icon = document.createElement('i')
+      icon.classList.add('bi')
+      icon.classList.add(column.isVisible() ? 'bi-eye' : 'bi-eye-slash')
+
+      //build label (contains icon and title)
+      let label = document.createElement('span')
+      let title = document.createElement('span')
+
+      title.innerHTML = ' ' + column.getDefinition().title
+
+      label.appendChild(icon)
+      label.appendChild(title)
+
+      // Take inital visibility into account
+      if (!column.isVisible()) {
+        label.classList.add('text-muted')
+      }
+
+      //create menu item
+      menu.push({
+        label: label,
+        action: function (e) {
+          //prevent menu closing
+          e.stopPropagation()
+
+          //toggle current column visibility
+          column.toggle()
+          //change menu item icon and toggle text-muted class based on visibility
+          label.classList.toggle('text-muted', !column.isVisible())
+          if (column.isVisible()) {
+            icon.classList.remove('bi-eye-slash')
+            icon.classList.add('bi-eye')
+          } else {
+            icon.classList.remove('bi-eye')
+            icon.classList.add('bi-eye-slash')
+          }
+        }
+      })
+    })
+
+    return menu
+  }
+
   var table = new Tabulator('#documents-table', {
     pagination: true,
     paginationSize: 15,
@@ -15,7 +64,8 @@ d3.json(dataUrl, function (data) {
         title: 'Shelfmark',
         field: 'shelfmark',
         headerFilter: 'input',
-        formatter: linkToDetailView
+        formatter: linkToDetailView,
+        headerMenu: headerMenu
       },
       {
         title: '<i>Bakkal</i>/Grocer',
@@ -26,7 +76,8 @@ d3.json(dataUrl, function (data) {
           idField: 'grocerist_id',
           nameField: 'name'
         },
-        headerFilterFuncParams: { nameField: 'name' }
+        headerFilterFuncParams: { nameField: 'name' },
+        headerMenu: headerMenu
       },
       {
         title: 'Transcript',
@@ -36,7 +87,8 @@ d3.json(dataUrl, function (data) {
         headerFilterParams: { tristate: true },
         headerFilterEmptyCheck: function (value) {
           return value === null
-        }
+        },
+        headerMenu: headerMenu
       },
       {
         title: 'Facsimiles',
@@ -46,7 +98,8 @@ d3.json(dataUrl, function (data) {
         headerFilterParams: { tristate: true },
         headerFilterEmptyCheck: function (value) {
           return value === null
-        }
+        },
+        headerMenu: headerMenu
       },
       {
         title: 'Groceries',
@@ -58,7 +111,8 @@ d3.json(dataUrl, function (data) {
           nameField: 'name'
         },
         ...linkListColumnSettings,
-        headerFilterFuncParams: { nameField: 'name' }
+        headerFilterFuncParams: { nameField: 'name' },
+        headerMenu: headerMenu
       },
       {
         title: 'District',
@@ -68,7 +122,8 @@ d3.json(dataUrl, function (data) {
           urlPrefix: 'district__',
           idField: 'id',
           nameField: 'value'
-        }
+        },
+        headerMenu: headerMenu
       },
       {
         title: '<i>Mahalle</i>',
@@ -79,7 +134,8 @@ d3.json(dataUrl, function (data) {
           urlPrefix: 'neighbourhood__',
           idField: 'id',
           nameField: 'value'
-        }
+        },
+        headerMenu: headerMenu
       },
       {
         title: '<i>Karye</i>',
@@ -90,7 +146,8 @@ d3.json(dataUrl, function (data) {
           urlPrefix: 'karye__',
           idField: 'id',
           nameField: 'value'
-        }
+        },
+        headerMenu: headerMenu
       },
       {
         title: '<i>Nahiye</i>',
@@ -101,7 +158,8 @@ d3.json(dataUrl, function (data) {
           urlPrefix: 'nahiye__',
           idField: 'id',
           nameField: 'value'
-        }
+        },
+        headerMenu: headerMenu
       },
       {
         title: 'Quarter',
@@ -112,7 +170,8 @@ d3.json(dataUrl, function (data) {
           urlPrefix: 'quarter__',
           idField: 'id',
           nameField: 'value'
-        }
+        },
+        headerMenu: headerMenu
       },
       {
         title: 'Address',
@@ -123,52 +182,25 @@ d3.json(dataUrl, function (data) {
           urlPrefix: 'address__',
           idField: 'id',
           nameField: 'value'
-        }
+        },
+        headerMenu: headerMenu
       },
       {
         title: 'Year <i>Hicri</i>',
         field: 'year_of_creation_hicri',
-        headerFilter: 'input'
+        headerFilter: 'input',
+        headerMenu: headerMenu
       },
       {
         title: 'Year <i>Miladi</i>',
         field: 'year_of_creation_miladi',
-        headerFilter: 'input'
+        headerFilter: 'input',
+        headerMenu: headerMenu
       }
     ],
-    footerElement:
-      '<span class="tabulator-counter float-left">' +
-      'Showing <span id="search_count"></span> results out of <span id="total_count"></span> ' +
-      '</span>'
-  })
-
-  function updateDropdownItems () {
-    const allColumns = table.getColumns()
-    const dropdownMenu = document.querySelector('#columnVisibility')
-    //dropdownMenu.innerHTML = '' // Clear existing dropdown items if necessary
-    // Add a dropdown item for each column
-    allColumns.forEach(column => {
-      const title = column.getDefinition().title
-      const item = document.createElement('a')
-      item.href = '#'
-      item.classList.add('dropdown-item')
-      // Add 'text-muted' class if the column is hidden
-      if (!column.isVisible()) {
-        item.classList.add('text-muted')
-      }
-      item.textContent = title
-      // Toggle column visibility (& text-muted class) when an item is clicked
-      item.onclick = function (event) {
-        event.preventDefault() // Prevent the default anchor action
-        column.toggle()
-        item.classList.toggle('text-muted', !column.isVisible())
-      }
-      dropdownMenu.appendChild(item)
-    })
-  }
-  // Populate the dropdown after the table is fully built
-  table.on('tableBuilt', function () {
-    updateDropdownItems()
+    footerElement: `<span class="tabulator-counter float-left">
+                    Showing <span id="search_count"></span> results out of <span id="total_count"></span>
+                    </span>`
   })
 
   table.on('dataLoaded', function (data) {
