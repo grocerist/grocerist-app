98095230580e94db6dcf54b4da0a5cc242b501ea
kisram <Kinga.Sramo@oeaw.ac.at>
2024-07-17 14:37:47 +0200

    responsiveness for documents table, refactoring

--- html/js/documents.js
+++ html/js/documents.js
@@ -1,213 +1,154 @@
-const dataUrl = 'json_dumps/documents.json'
+const dataUrl = "json_dumps/documents.json";
+const baseColumnDefinitions = [
+  {
+    title: "Shelfmark",
+    field: "shelfmark",
+    headerFilter: "input",
+    formatter: linkToDetailView,
+  },
+  {
+    title: "<i>Bakkal</i>/Grocer",
+    field: "main_person",
+    ...linkListColumnSettings,
+    formatterParams: {
+      urlPrefix: "",
+      idField: "grocerist_id",
+      nameField: "name",
+    },
+    headerFilterFuncParams: { nameField: "name" },
+  },
+  {
+    title: "Transcript",
+    field: "transcript",
+    formatter: "tickCross",
+    headerFilter: "tickCross",
+    headerFilterParams: { tristate: true },
+    headerFilterEmptyCheck: function (value) {
+      return value === null;
+    },
+  },
+  {
+    title: "Facsimiles",
+    field: "images",
+    formatter: "tickCross",
+    headerFilter: "tickCross",
+    headerFilterParams: { tristate: true },
+    headerFilterEmptyCheck: function (value) {
+      return value === null;
+    },
+  },
+  {
+    title: "Groceries",
+    field: "goods",
+    formatterParams: {
+      scrollable: true,
+      urlPrefix: "",
+      idField: "grocerist_id",
+      nameField: "name",
+    },
+    ...linkListColumnSettings,
+    headerFilterFuncParams: { nameField: "name" },
+  },
+  {
+    title: "District",
+    field: "district",
+    ...linkListColumnSettings,
+    formatterParams: {
+      urlPrefix: "district__",
+      idField: "id",
+      nameField: "value",
+    },
+  },
+  {
+    title: "<i>Mahalle</i>",
+    field: "neighbourhood",
+    visible: false,
+    ...linkListColumnSettings,
+    formatterParams: {
+      urlPrefix: "neighbourhood__",
+      idField: "id",
+      nameField: "value",
+    },
+  },
+  {
+    title: "<i>Karye</i>",
+    field: "karye",
+    visible: false,
+    ...linkListColumnSettings,
+    formatterParams: {
+      urlPrefix: "karye__",
+      idField: "id",
+      nameField: "value",
+    },
+  },
+  {
+    title: "<i>Nahiye</i>",
+    field: "nahiye",
+    visible: false,
+    ...linkListColumnSettings,
+    formatterParams: {
+      urlPrefix: "nahiye__",
+      idField: "id",
+      nameField: "value",
+    },
+  },
+  {
+    title: "Quarter",
+    field: "quarter",
+    visible: false,
+    ...linkListColumnSettings,
+    formatterParams: {
+      urlPrefix: "quarter__",
+      idField: "id",
+      nameField: "value",
+    },
+  },
+  {
+    title: "Address",
+    field: "address",
+    visible: false,
+    ...linkListColumnSettings,
+    formatterParams: {
+      urlPrefix: "address__",
+      idField: "id",
+      nameField: "value",
+    },
+  },
+  {
+    title: "Year <i>Hicri</i>",
+    field: "year_of_creation_hicri",
+    headerFilter: "input",
+  },
+  {
+    title: "Year <i>Miladi</i>",
+    field: "year_of_creation_miladi",
+    headerFilter: "input",
+  },
+];
+// Add minWidth and visibility toggle to each column
+const columnDefinitions = baseColumnDefinitions.map((column) => ({
+  ...column,
+  headerMenu: headerMenu,
+  minWidth: 200,
+}));
 
 d3.json(dataUrl, function (data) {
-  tableData = Object.values(data)
+  tableData = Object.values(data);
 
-  //define column header menu as column visibility toggle
-  const headerMenu = function () {
-    let menu = []
-    const allColumns = this.getColumns()
-
-    allColumns.forEach(column => {
-      let icon = document.createElement('i')
-      icon.classList.add('bi')
-      icon.classList.add(column.isVisible() ? 'bi-eye' : 'bi-eye-slash')
-
-      //build label (contains icon and title)
-      let label = document.createElement('span')
-      let title = document.createElement('span')
-
-      title.innerHTML = ' ' + column.getDefinition().title
-
-      label.appendChild(icon)
-      label.appendChild(title)
-
-      // Take inital visibility into account
-      if (!column.isVisible()) {
-        label.classList.add('text-muted')
-      }
-
-      //create menu item
-      menu.push({
-        label: label,
-        action: function (e) {
-          //prevent menu closing
-          e.stopPropagation()
-
-          //toggle current column visibility
-          column.toggle()
-          //change menu item icon and toggle text-muted class based on visibility
-          label.classList.toggle('text-muted', !column.isVisible())
-          if (column.isVisible()) {
-            icon.classList.remove('bi-eye-slash')
-            icon.classList.add('bi-eye')
-          } else {
-            icon.classList.remove('bi-eye')
-            icon.classList.add('bi-eye-slash')
-          }
-        }
-      })
-    })
-
-    return menu
-  }
-
-  var table = new Tabulator('#documents-table', {
-    pagination: true,
-    paginationSize: 15,
-    layout: 'fitDataStretch',
-    height: 800,
-    tooltips: true,
+  var table = new Tabulator("#documents-table", {
+    ...commonTableConfig,
     data: tableData,
-    columns: [
-      {
-        title: 'Shelfmark',
-        field: 'shelfmark',
-        headerFilter: 'input',
-        formatter: linkToDetailView,
-        headerMenu: headerMenu
-      },
-      {
-        title: '<i>Bakkal</i>/Grocer',
-        field: 'main_person',
-        ...linkListColumnSettings,
-        formatterParams: {
-          urlPrefix: '',
-          idField: 'grocerist_id',
-          nameField: 'name'
-        },
-        headerFilterFuncParams: { nameField: 'name' },
-        headerMenu: headerMenu
-      },
-      {
-        title: 'Transcript',
-        field: 'transcript',
-        formatter: 'tickCross',
-        headerFilter: 'tickCross',
-        headerFilterParams: { tristate: true },
-        headerFilterEmptyCheck: function (value) {
-          return value === null
-        },
-        headerMenu: headerMenu
-      },
-      {
-        title: 'Facsimiles',
-        field: 'images',
-        formatter: 'tickCross',
-        headerFilter: 'tickCross',
-        headerFilterParams: { tristate: true },
-        headerFilterEmptyCheck: function (value) {
-          return value === null
-        },
-        headerMenu: headerMenu
-      },
-      {
-        title: 'Groceries',
-        field: 'goods',
-        formatterParams: {
-          scrollable: true,
-          urlPrefix: '',
-          idField: 'grocerist_id',
-          nameField: 'name'
-        },
-        ...linkListColumnSettings,
-        headerFilterFuncParams: { nameField: 'name' },
-        headerMenu: headerMenu
-      },
-      {
-        title: 'District',
-        field: 'district',
-        ...linkListColumnSettings,
-        formatterParams: {
-          urlPrefix: 'district__',
-          idField: 'id',
-          nameField: 'value'
-        },
-        headerMenu: headerMenu
-      },
-      {
-        title: '<i>Mahalle</i>',
-        field: 'neighbourhood',
-        visible: false,
-        ...linkListColumnSettings,
-        formatterParams: {
-          urlPrefix: 'neighbourhood__',
-          idField: 'id',
-          nameField: 'value'
-        },
-        headerMenu: headerMenu
-      },
-      {
-        title: '<i>Karye</i>',
-        field: 'karye',
-        visible: false,
-        ...linkListColumnSettings,
-        formatterParams: {
-          urlPrefix: 'karye__',
-          idField: 'id',
-          nameField: 'value'
-        },
-        headerMenu: headerMenu
-      },
-      {
-        title: '<i>Nahiye</i>',
-        field: 'nahiye',
-        visible: false,
-        ...linkListColumnSettings,
-        formatterParams: {
-          urlPrefix: 'nahiye__',
-          idField: 'id',
-          nameField: 'value'
-        },
-        headerMenu: headerMenu
-      },
-      {
-        title: 'Quarter',
-        field: 'quarter',
-        visible: false,
-        ...linkListColumnSettings,
-        formatterParams: {
-          urlPrefix: 'quarter__',
-          idField: 'id',
-          nameField: 'value'
-        },
-        headerMenu: headerMenu
-      },
-      {
-        title: 'Address',
-        field: 'address',
-        visible: false,
-        ...linkListColumnSettings,
-        formatterParams: {
-          urlPrefix: 'address__',
-          idField: 'id',
-          nameField: 'value'
-        },
-        headerMenu: headerMenu
-      },
-      {
-        title: 'Year <i>Hicri</i>',
-        field: 'year_of_creation_hicri',
-        headerFilter: 'input',
-        headerMenu: headerMenu
-      },
-      {
-        title: 'Year <i>Miladi</i>',
-        field: 'year_of_creation_miladi',
-        headerFilter: 'input',
-        headerMenu: headerMenu
-      }
-    ],
+    columns: columnDefinitions,
     footerElement: `<span class="tabulator-counter float-left">
                     Showing <span id="search_count"></span> results out of <span id="total_count"></span>
-                    </span>`
-  })
+                    </span>`,
+  });
 
-  table.on('dataLoaded', function (data) {
-    $('#total_count').text(data.length)
-  })
+  table.on("dataLoaded", function (data) {
+    $("#total_count").text(data.length);
+  });
 
-  table.on('dataFiltered', function (filters, rows) {
-    $('#search_count').text(rows.length)
-  })
-})
+  table.on("dataFiltered", function (filters, rows) {
+    $("#search_count").text(rows.length);
+  });
+});
--- html/js/tabulator-utils.js
+++ html/js/tabulator-utils.js
@@ -1,93 +1,153 @@
-function get_scrollable_cell (renderer, cell, cell_html_string) {
+// Description: Settings and utility functions for Tabulator tables
+
+// common settings for all Tabulator tables
+const commonTableConfig = {
+  layout: "fitColumns",
+  responsiveLayout: "collapse",
+  height: 800,
+  // width: "100%",
+  pagination: true,
+  paginationSize: 15,
+};
+
+// common settings for columns with arrays of objects
+const linkListColumnSettings = {
+  formatter: linkListFormatter,
+  headerFilter: "input",
+  headerFilterFunc: customHeaderFilter,
+  sorter: "array",
+};
+
+function get_scrollable_cell(renderer, cell, cell_html_string) {
   // by @cfhaak, https://github.com/NestroyCA/nestroyca-astro-base/blob/main/src/pages/places.astro#L80
   if (cell_html_string === undefined) {
-    cell_html_string = cell.getValue()
+    cell_html_string = cell.getValue();
   }
-  var cell_html_element = cell.getElement()
-  cell_html_element.style.whiteSpace = 'pre-wrap'
-  cell_html_element.style.overflow = 'auto'
-  cell_html_element.style.maxHeight = '100px'
-  let final_val = renderer.emptyToSpace(cell_html_string)
-  return final_val
+  var cell_html_element = cell.getElement();
+  cell_html_element.style.whiteSpace = "pre-wrap";
+  cell_html_element.style.overflow = "auto";
+  cell_html_element.style.maxHeight = "100px";
+  let final_val = renderer.emptyToSpace(cell_html_string);
+  return final_val;
 }
 
-function linkListFormatter (cell, formatterParams, onRendered) {
-  let value = cell.getValue()
+function linkListFormatter(cell, formatterParams, onRendered) {
+  let value = cell.getValue();
   let output = value
-    .map(item => {
+    .map((item) => {
       return `<li><a href="${formatterParams.urlPrefix}${
         item[formatterParams.idField]
-      }.html">${item[formatterParams.nameField]}</a></li>`
+      }.html">${item[formatterParams.nameField]}</a></li>`;
     })
-    .join(' ')
-  output = `<ul class="list-unstyled">${output}</ul>`
-  let renderer = this
+    .join(" ");
+  output = `<ul class="list-unstyled">${output}</ul>`;
+  let renderer = this;
   if (formatterParams.scrollable === true) {
-    output = get_scrollable_cell(renderer, cell, output)
+    output = get_scrollable_cell(renderer, cell, output);
   }
-  return output
+  return output;
 }
 
-function mutateSelectField (value, data, type, params, component) {
+function mutateSelectField(value, data, type, params, component) {
   let output = value
-    .map(item => {
-      return `${item.value}`
+    .map((item) => {
+      return `${item.value}`;
     })
-    .join('/')
-  return `${output}`
+    .join("/");
+  return `${output}`;
 }
 
-function makeItalic (value) {
-  let turkishWords = ['Nahiye', 'Mahalle', 'Karye']
-  if (typeof value === 'string') {
+function makeItalic(value) {
+  let turkishWords = ["Nahiye", "Mahalle", "Karye"];
+  if (typeof value === "string") {
     //headerfilter case
-    output = value
+    output = value;
   } else {
     //tab cell case
-    output = value.getValue()
+    output = value.getValue();
   }
   if (turkishWords.includes(output)) {
-    output = '<i>' + output + '</i>'
+    output = "<i>" + output + "</i>";
   }
-  return output
+  return output;
 }
 
 // for the first column, the name is a link to the detail view
-function linkToDetailView (cell) {
-  var row = cell.getRow().getData()
-  var cellData = cell.getData()
-  var groceristId = row.grocerist_id
+function linkToDetailView(cell) {
+  var row = cell.getRow().getData();
+  var cellData = cell.getData();
+  var groceristId = row.grocerist_id;
   // for documents, name = shelfmark
-  var linkText = cellData.name ? cellData.name : cellData.shelfmark
-  var theLink = `<a href="${groceristId}.html">${linkText}</a>`
+  var linkText = cellData.name ? cellData.name : cellData.shelfmark;
+  var theLink = `<a href="${groceristId}.html">${linkText}</a>`;
   // for locations, the id is in the properties (geoJSON)
   if (groceristId === undefined) {
-    groceristId = row.properties.grocerist_id
-    theLink = `<a href="${groceristId}.html">${cellData.properties.name}</a>`
+    groceristId = row.properties.grocerist_id;
+    theLink = `<a href="${groceristId}.html">${cellData.properties.name}</a>`;
   }
-  return theLink
+  return theLink;
 }
 // custom headerFilter for cells with arrays of objects
-function customHeaderFilter (headerValue, rowValue, rowData, filterParams) {
+function customHeaderFilter(headerValue, rowValue, rowData, filterParams) {
   // for columns where the name of the items is not in the "value" field,
   // the line headerFilterFuncParams: { nameField: 'name' } needs to be added to the column config
   if (filterParams.nameField) {
     return rowValue.some(function (item) {
       return item[filterParams.nameField]
         .toLowerCase()
-        .includes(headerValue.toLowerCase())
-    })
+        .includes(headerValue.toLowerCase());
+    });
   } else {
     return rowValue.some(function (item) {
-      return item.value.toLowerCase().includes(headerValue.toLowerCase())
-    })
+      return item.value.toLowerCase().includes(headerValue.toLowerCase());
+    });
   }
 }
 
-// common settings for columns with arrays of objects
-const linkListColumnSettings = {
-  formatter: linkListFormatter,
-  headerFilter: 'input',
-  headerFilterFunc: customHeaderFilter,
-  sorter: 'array'
-}
+// Define column header menu as column visibility toggle
+const headerMenu = function () {
+  let menu = [];
+  const allColumns = this.getColumns();
+  table = this; // to access the table instance in the menu item action
+
+  allColumns.forEach((column) => {
+    let icon = document.createElement("i");
+    icon.className = "bi " + (column.isVisible() ? "bi-eye" : "bi-eye-slash");
+
+    //build label (contains icon and title)
+    let label = document.createElement("span");
+    let title = document.createElement("span");
+
+    title.innerHTML = " " + column.getDefinition().title;
+
+    label.appendChild(icon);
+    label.appendChild(title);
+
+    // Take inital visibility into account
+    if (!column.isVisible()) {
+      label.classList.add("text-muted");
+    }
+
+    //create menu item
+    menu.push({
+      label: label,
+      action: function (e) {
+        // Prevent menu closing
+        e.stopPropagation();
+
+        // Toggle current column visibility
+        column.toggle();
+
+        // Redraw the table, potentially uncollapsing columns
+        table.redraw();
+
+        // Change menu item icon and toggle text-muted class based on visibility
+        label.classList.toggle("text-muted", !column.isVisible());
+        icon.className =
+          "bi " + (column.isVisible() ? "bi-eye" : "bi-eye-slash");
+      },
+    });
+  });
+
+  return menu;
+};
