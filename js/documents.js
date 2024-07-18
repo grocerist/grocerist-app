041e8e8f975bd94239011055ad34bdb56eba03e6
kisram <kisram@users.noreply.github.com>
2024-07-18 14:11:48 +0000

    deploy: 819cc1fcec9a0fbc86d67365f6b062f35c85b3dc

--- js/documents.js
+++ js/documents.js
@@ -1,30 +1,30 @@
-const dataUrl = 'json_dumps/documents.json'
+const dataUrl = "json_dumps/documents.json";
 
 d3.json(dataUrl, function (data) {
-  tableData = Object.values(data)
+  tableData = Object.values(data).filter((item) => item.shelfmark !== "");
 
   //define column header menu as column visibility toggle
   const headerMenu = function () {
-    let menu = []
-    const allColumns = this.getColumns()
+    let menu = [];
+    const allColumns = this.getColumns();
 
-    allColumns.forEach(column => {
-      let icon = document.createElement('i')
-      icon.classList.add('bi')
-      icon.classList.add(column.isVisible() ? 'bi-eye' : 'bi-eye-slash')
+    allColumns.forEach((column) => {
+      let icon = document.createElement("i");
+      icon.classList.add("bi");
+      icon.classList.add(column.isVisible() ? "bi-eye" : "bi-eye-slash");
 
       //build label (contains icon and title)
-      let label = document.createElement('span')
-      let title = document.createElement('span')
+      let label = document.createElement("span");
+      let title = document.createElement("span");
 
-      title.innerHTML = ' ' + column.getDefinition().title
+      title.innerHTML = " " + column.getDefinition().title;
 
-      label.appendChild(icon)
-      label.appendChild(title)
+      label.appendChild(icon);
+      label.appendChild(title);
 
       // Take inital visibility into account
       if (!column.isVisible()) {
-        label.classList.add('text-muted')
+        label.classList.add("text-muted");
       }
 
       //create menu item
@@ -32,182 +32,182 @@ d3.json(dataUrl, function (data) {
         label: label,
         action: function (e) {
           //prevent menu closing
-          e.stopPropagation()
+          e.stopPropagation();
 
           //toggle current column visibility
-          column.toggle()
+          column.toggle();
           //change menu item icon and toggle text-muted class based on visibility
-          label.classList.toggle('text-muted', !column.isVisible())
+          label.classList.toggle("text-muted", !column.isVisible());
           if (column.isVisible()) {
-            icon.classList.remove('bi-eye-slash')
-            icon.classList.add('bi-eye')
+            icon.classList.remove("bi-eye-slash");
+            icon.classList.add("bi-eye");
           } else {
-            icon.classList.remove('bi-eye')
-            icon.classList.add('bi-eye-slash')
+            icon.classList.remove("bi-eye");
+            icon.classList.add("bi-eye-slash");
           }
-        }
-      })
-    })
+        },
+      });
+    });
 
-    return menu
-  }
+    return menu;
+  };
 
-  var table = new Tabulator('#documents-table', {
+  var table = new Tabulator("#documents-table", {
     pagination: true,
     paginationSize: 15,
-    layout: 'fitDataStretch',
+    layout: "fitDataStretch",
     height: 800,
     tooltips: true,
     data: tableData,
     columns: [
       {
-        title: 'Shelfmark',
-        field: 'shelfmark',
-        headerFilter: 'input',
+        title: "Shelfmark",
+        field: "shelfmark",
+        headerFilter: "input",
         formatter: linkToDetailView,
-        headerMenu: headerMenu
+        headerMenu: headerMenu,
       },
       {
-        title: '<i>Bakkal</i>/Grocer',
-        field: 'main_person',
+        title: "<i>Bakkal</i>/Grocer",
+        field: "main_person",
         ...linkListColumnSettings,
         formatterParams: {
-          urlPrefix: '',
-          idField: 'grocerist_id',
-          nameField: 'name'
+          urlPrefix: "",
+          idField: "grocerist_id",
+          nameField: "name",
         },
-        headerFilterFuncParams: { nameField: 'name' },
-        headerMenu: headerMenu
+        headerFilterFuncParams: { nameField: "name" },
+        headerMenu: headerMenu,
       },
       {
-        title: 'Transcript',
-        field: 'transcript',
-        formatter: 'tickCross',
-        headerFilter: 'tickCross',
+        title: "Transcript",
+        field: "transcript",
+        formatter: "tickCross",
+        headerFilter: "tickCross",
         headerFilterParams: { tristate: true },
         headerFilterEmptyCheck: function (value) {
-          return value === null
+          return value === null;
         },
-        headerMenu: headerMenu
+        headerMenu: headerMenu,
       },
       {
-        title: 'Facsimiles',
-        field: 'images',
-        formatter: 'tickCross',
-        headerFilter: 'tickCross',
+        title: "Facsimiles",
+        field: "images",
+        formatter: "tickCross",
+        headerFilter: "tickCross",
         headerFilterParams: { tristate: true },
         headerFilterEmptyCheck: function (value) {
-          return value === null
+          return value === null;
         },
-        headerMenu: headerMenu
+        headerMenu: headerMenu,
       },
       {
-        title: 'Groceries',
-        field: 'goods',
+        title: "Groceries",
+        field: "goods",
         formatterParams: {
           scrollable: true,
-          urlPrefix: '',
-          idField: 'grocerist_id',
-          nameField: 'name'
+          urlPrefix: "",
+          idField: "grocerist_id",
+          nameField: "name",
         },
         ...linkListColumnSettings,
-        headerFilterFuncParams: { nameField: 'name' },
-        headerMenu: headerMenu
+        headerFilterFuncParams: { nameField: "name" },
+        headerMenu: headerMenu,
       },
       {
-        title: 'District',
-        field: 'district',
+        title: "District",
+        field: "district",
         ...linkListColumnSettings,
         formatterParams: {
-          urlPrefix: 'district__',
-          idField: 'id',
-          nameField: 'value'
+          urlPrefix: "district__",
+          idField: "id",
+          nameField: "value",
         },
-        headerMenu: headerMenu
+        headerMenu: headerMenu,
       },
       {
-        title: '<i>Mahalle</i>',
-        field: 'neighbourhood',
+        title: "<i>Mahalle</i>",
+        field: "neighbourhood",
         visible: false,
         ...linkListColumnSettings,
         formatterParams: {
-          urlPrefix: 'neighbourhood__',
-          idField: 'id',
-          nameField: 'value'
+          urlPrefix: "neighbourhood__",
+          idField: "id",
+          nameField: "value",
         },
-        headerMenu: headerMenu
+        headerMenu: headerMenu,
       },
       {
-        title: '<i>Karye</i>',
-        field: 'karye',
+        title: "<i>Karye</i>",
+        field: "karye",
         visible: false,
         ...linkListColumnSettings,
         formatterParams: {
-          urlPrefix: 'karye__',
-          idField: 'id',
-          nameField: 'value'
+          urlPrefix: "karye__",
+          idField: "id",
+          nameField: "value",
         },
-        headerMenu: headerMenu
+        headerMenu: headerMenu,
       },
       {
-        title: '<i>Nahiye</i>',
-        field: 'nahiye',
+        title: "<i>Nahiye</i>",
+        field: "nahiye",
         visible: false,
         ...linkListColumnSettings,
         formatterParams: {
-          urlPrefix: 'nahiye__',
-          idField: 'id',
-          nameField: 'value'
+          urlPrefix: "nahiye__",
+          idField: "id",
+          nameField: "value",
         },
-        headerMenu: headerMenu
+        headerMenu: headerMenu,
       },
       {
-        title: 'Quarter',
-        field: 'quarter',
+        title: "Quarter",
+        field: "quarter",
         visible: false,
         ...linkListColumnSettings,
         formatterParams: {
-          urlPrefix: 'quarter__',
-          idField: 'id',
-          nameField: 'value'
+          urlPrefix: "quarter__",
+          idField: "id",
+          nameField: "value",
         },
-        headerMenu: headerMenu
+        headerMenu: headerMenu,
       },
       {
-        title: 'Address',
-        field: 'address',
+        title: "Address",
+        field: "address",
         visible: false,
         ...linkListColumnSettings,
         formatterParams: {
-          urlPrefix: 'address__',
-          idField: 'id',
-          nameField: 'value'
+          urlPrefix: "address__",
+          idField: "id",
+          nameField: "value",
         },
-        headerMenu: headerMenu
+        headerMenu: headerMenu,
       },
       {
-        title: 'Year <i>Hicri</i>',
-        field: 'year_of_creation_hicri',
-        headerFilter: 'input',
-        headerMenu: headerMenu
+        title: "Year <i>Hicri</i>",
+        field: "year_of_creation_hicri",
+        headerFilter: "input",
+        headerMenu: headerMenu,
       },
       {
-        title: 'Year <i>Miladi</i>',
-        field: 'year_of_creation_miladi',
-        headerFilter: 'input',
-        headerMenu: headerMenu
-      }
+        title: "Year <i>Miladi</i>",
+        field: "year_of_creation_miladi",
+        headerFilter: "input",
+        headerMenu: headerMenu,
+      },
     ],
     footerElement: `<span class="tabulator-counter float-left">
                     Showing <span id="search_count"></span> results out of <span id="total_count"></span>
-                    </span>`
-  })
+                    </span>`,
+  });
 
-  table.on('dataLoaded', function (data) {
-    $('#total_count').text(data.length)
-  })
+  table.on("dataLoaded", function (data) {
+    $("#total_count").text(data.length);
+  });
 
-  table.on('dataFiltered', function (filters, rows) {
-    $('#search_count').text(rows.length)
-  })
-})
+  table.on("dataFiltered", function (filters, rows) {
+    $("#search_count").text(rows.length);
+  });
+});
--- js/goods.js
+++ js/goods.js
@@ -1,64 +1,64 @@
-const dataUrl = 'json_dumps/goods.json'
+const dataUrl = "json_dumps/goods.json";
 
 d3.json(dataUrl, function (data) {
-  data = Object.values(data)
-  let tableData = data.map(item => {
-    const enriched = item
-    enriched['doc_count'] = item.documents.length
-    enriched['cat_cont'] = item.has_category.length
-    return enriched
-  })
+  data = Object.values(data).filter((item) => item.name !== "");
+  let tableData = data.map((item) => {
+    const enriched = item;
+    enriched["doc_count"] = item.documents.length;
+    enriched["cat_cont"] = item.has_category.length;
+    return enriched;
+  });
 
-  var table = new Tabulator('#example-table', {
+  var table = new Tabulator("#example-table", {
     pagination: true,
     paginationSize: 15,
-    layout: 'fitDataStretch',
-    responsiveLayout: 'collapse',
+    layout: "fitDataStretch",
+    responsiveLayout: "collapse",
     height: 800,
     tooltips: true,
     data: tableData,
     columns: [
       {
-        title: 'Name',
-        field: 'name',
-        headerFilter: 'input',
-        formatter: linkToDetailView
+        title: "Name",
+        field: "name",
+        headerFilter: "input",
+        formatter: linkToDetailView,
       },
       {
-        title: 'Documents',
-        field: 'documents',
+        title: "Documents",
+        field: "documents",
         ...linkListColumnSettings,
         formatterParams: {
           scrollable: true,
-          urlPrefix: 'document__',
-          idField: 'id',
-          nameField: 'value'
+          urlPrefix: "document__",
+          idField: "id",
+          nameField: "value",
         },
       },
       {
-        title: 'Nr. of Documents',
-        field: 'doc_count',
-        headerFilter: 'number',
-        headerFilterPlaceholder: 'at least...',
-        headerFilterFunc: '>='
+        title: "Nr. of Documents",
+        field: "doc_count",
+        headerFilter: "number",
+        headerFilterPlaceholder: "at least...",
+        headerFilterFunc: ">=",
       },
       {
-        title: 'Categories',
-        field: 'has_category',
+        title: "Categories",
+        field: "has_category",
         ...linkListColumnSettings,
         formatterParams: {
-          urlPrefix: 'category__',
-          idField: 'id',
-          nameField: 'value'
+          urlPrefix: "category__",
+          idField: "id",
+          nameField: "value",
         },
       },
       {
-        title: 'Nr. of Categories',
-        field: 'cat_cont',
-        headerFilter: 'number',
-        headerFilterPlaceholder: 'at least...',
-        headerFilterFunc: '>='
-      }
-    ]
-  })
-})
+        title: "Nr. of Categories",
+        field: "cat_cont",
+        headerFilter: "number",
+        headerFilterPlaceholder: "at least...",
+        headerFilterFunc: ">=",
+      },
+    ],
+  });
+});
