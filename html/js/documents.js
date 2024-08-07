6ae7b6290d0c9202e49ff7a080002bb5cafdb80d
kisram <Kinga.Sramo@oeaw.ac.at>
2024-08-07 11:26:13 +0200

    work in progress, works except for marker popups

--- html/js/documents.js
+++ html/js/documents.js
@@ -1,4 +1,73 @@
 const dataUrl = "json_dumps/documents.json";
+// ####### MAP CONFIG AND FUNCTIONS #######
+// Config settings for map
+const mapConfig = {
+  initialZoom: 10,
+  initialCoordinates: [41.01224, 28.976018],
+  baseMapUrl: "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
+  maxZoom: 20,
+  minZoom: 1,
+  onRowClickZoom: 16,
+  divId: "map",
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+  subdomains: "abcd",
+};
+
+// Helper function to create and add layer groups to the map
+const createAndAddLayerGroup = (map, name) => {
+  const layerGroup = new L.layerGroup();
+  layerGroup.addTo(map);
+  return { [name]: layerGroup };
+};
+
+// Function for initializing the map
+function createMap() {
+  console.log("loading map");
+  const map = L.map(mapConfig.divId).setView(
+    mapConfig.initialCoordinates,
+    mapConfig.initialZoom
+  );
+  const baseMapLayer = L.tileLayer(mapConfig.baseMapUrl, {
+    maxZoom: mapConfig.maxZoom,
+    attribution: mapConfig.attribution,
+  });
+  // Add base map layer
+  baseMapLayer.addTo(map);
+
+  // Create and add marker layers
+  const layerGroups = {
+    ...createAndAddLayerGroup(map, "18th century"),
+    ...createAndAddLayerGroup(map, "19th century"),
+    ...createAndAddLayerGroup(map, "No data"),
+  };
+
+  const layerControl = L.control.layers(null, layerGroups, {
+    collapsed: false,
+  });
+  layerControl.addTo(map);
+  // addLegend(map);
+  return { map, layerGroups };
+}
+// Function to create a marker
+function createMarker(lat, long, radius, color, markerID) {
+  return L.circleMarker([lat, long], {
+    radius: radius,
+    color: color,
+    markerID: markerID,
+  });
+}
+function addPopup(rowData) {
+  let popupContent = `
+    <h3><a href="${rowData.grocerist_id}.html">${rowData.shelfmark}<a/></h3>
+    <p><b><i>Bakkal</i>/Grocer:</b> ${
+      rowData.main_person[0] ? rowData.main_person[0].name : "-"
+    }</p>
+    `;
+  return popupContent;
+}
+
+// ####### TABLE CONFIG AND FUNCTIONS #######
 const baseColumnDefinitions = [
   {
     title: "Shelfmark",
@@ -20,6 +89,7 @@ const baseColumnDefinitions = [
   {
     title: "Transcript",
     field: "transcript",
+    hozAlign: "center",
     formatter: "tickCross",
     headerFilter: "tickCross",
     headerFilterParams: { tristate: true },
@@ -30,6 +100,7 @@ const baseColumnDefinitions = [
   {
     title: "Facsimiles",
     field: "images",
+    hozAlign: "center",
     formatter: "tickCross",
     headerFilter: "tickCross",
     headerFilterParams: { tristate: true },
@@ -120,35 +191,130 @@ const baseColumnDefinitions = [
     headerFilter: "input",
   },
   {
-    title: "Year <i>Miladi</i>",
+    title: "Date <i>Miladi</i>",
     field: "year_of_creation_miladi",
     headerFilter: "input",
   },
 ];
 // Add minWidth and visibility toggle to each column
-const columnDefinitions = baseColumnDefinitions.map((column) => ({
-  ...column,
-  headerMenu: headerMenu,
-  minWidth: 200,
-}));
+const columnDefinitions = baseColumnDefinitions.map((column) => {
+  // List of columns that should not have minWidth
+  const narrowColumns = ["transcript", "images"];
+  const narrowMinWidth = 100;
+  const defaultMinWidth = 150;
 
-d3.json(dataUrl, function (data) {
- tableData = Object.values(data).filter((item) => item.shelfmark !== "");
+  // Determine the minWidth for the current column
+  const minWidth = narrowColumns.includes(column.field)
+    ? narrowMinWidth
+    : defaultMinWidth;
 
-  var table = new Tabulator("#documents-table", {
-    ...commonTableConfig,
-    data: tableData,
-    columns: columnDefinitions,
-    footerElement: `<span class="tabulator-counter float-left">
+  return {
+    ...column,
+    headerMenu: headerMenu,
+    minWidth: minWidth,
+  };
+});
+
+// Config settings for table
+const tableConfig = {
+  ...commonTableConfig,
+  headerFilterLiveFilterDelay: 600,
+  columns: columnDefinitions,
+  // initialSort: [{ column: "properties.name", dir: "asc" }],
+  footerElement: `<span class="tabulator-counter float-left">
                     Showing <span id="search_count"></span> results out of <span id="total_count"></span>
                     </span>`,
-  });
+};
 
-  table.on("dataLoaded", function (data) {
-    $("#total_count").text(data.length);
-  });
+// Function for creating table
+function createTable(tableConfig) {
+  console.log("loading table");
+  const table = new Tabulator("#documents-table", tableConfig);
+  return table;
+}
+// ###### MAP AND TABLE INTERACTION #######
+// Function to get coordinate key from row data
+function getCoordinates(rowData) {
+  return { lat: rowData.lat, long: rowData.long };
+}
 
-  table.on("dataFiltered", function (filters, rows) {
-    $("#search_count").text(rows.length);
+function rowsToMarkers(rows, layerGroups) {
+  markers = {};
+  // Clear all markers from the map and layer groups
+  Object.values(layerGroups).forEach((layerGroup) => {
+    layerGroup.clearLayers();
   });
-});
+  rows.forEach((row) => {
+    let rowData = row.getData();
+    let { lat, long } = getCoordinates(rowData);
+    if (lat && long) {
+      let year;
+      if (rowData.year_of_creation_miladi) {
+        year = /\d{4}/.exec(rowData.year_of_creation_miladi)[0];
+      } else {
+        year = 2000;
+      }
+      let color, radius, layer;
+      if (year <= 1800) {
+        color = "#2b84ad";
+        radius = 5;
+        layer = "18th century";
+      } else if (year <= 1900) {
+        color = "#247d04";
+        radius = 10;
+        layer = "19th century";
+      } else {
+        color = "#000";
+        radius = 2;
+        layer = "No data";
+      }
+      let marker = createMarker(lat, long, radius, color);
+
+      // store each marker by the grocerist_id from the document
+      let markerID = rowData.grocerist_id;
+      markers[markerID] = marker;
+
+      // marker.bindPopup(label);
+      layerGroups[layer].addLayer(marker);
+    }
+  });
+  return markers;
+}
+function zoomToPointFromRowData(rowData, map, markers) {
+  let { lat, long } = getCoordinates(rowData);
+  if (lat && long) {
+    let markerId = rowData.grocerist_id;
+    let marker = markers[markerId];
+    // marker.openPopup();
+    map.setView([lat, long], mapConfig.onRowClickZoom);
+  } else {
+    // close all open popups when resetting the map
+    // map.closePopup();
+    map.setView(mapConfig.initialCoordinates, mapConfig.initialZoom);
+  }
+}
+// Main function for initializing the map and table
+function setupMapAndTable(dataUrl) {
+  const { map, layerGroups } = createMap();
+  let markers = {};
+  d3.json(dataUrl, function (dataFromJson) {
+    const tableData = Object.values(dataFromJson).filter(
+      (item) => item.shelfmark !== ""
+    );
+    tableConfig.data = tableData;
+    const table = createTable(tableConfig);
+    table.on("dataLoaded", function (data) {
+      $("#total_count").text(data.length);
+    });
+    table.on("dataFiltered", function (_filters, rows) {
+      $("#search_count").text(rows.length);
+      markers = rowsToMarkers(rows, layerGroups);
+    });
+    //eventlistener for click on row
+    table.on("rowClick", (e, row) => {
+      zoomToPointFromRowData(row.getData(), map, markers);
+    });
+  });
+}
+
+setupMapAndTable(dataUrl);
