745cbfb4245555099f92d4e0d9972c0361cf8dc7
kisram <Kinga.Sramo@oeaw.ac.at>
2024-08-21 15:56:46 +0200

    update to current version of categories.js

--- html/js/categories.js
+++ html/js/categories.js
@@ -1,58 +1,58 @@
-const dataUrl = 'json_dumps/categories.json'
+const dataUrl = "json_dumps/categories.json";
 
 d3.json(dataUrl, function (data) {
-  tableData = Object.values(data)
+  tableData = Object.values(data);
 
-  var table = new Tabulator('#example-table', {
+  var table = new Tabulator("#example-table", {
     height: 800,
-    layout: 'fitColumns',
+    layout: "fitColumns",
     tooltips: true,
     data: tableData,
     pagination: true,
     paginationSize: 15,
-    responsiveLayout: 'collapse',
-    columnCalcs: 'both',
+    responsiveLayout: "collapse",
+    columnCalcs: "both",
     columns: [
       {
-        title: 'Grocery Category',
-        field: 'name',
-        headerFilter: 'input',
-        formatter:  linkToDetailView,
-        bottomCalc: 'count'
+        title: "Grocery Category",
+        field: "name",
+        headerFilter: "input",
+        formatter: linkToDetailView,
+        bottomCalc: "count",
       },
       {
-        title: 'Groceries',
-        field: 'goods',
+        title: "Groceries",
+        field: "goods",
         ...linkListColumnSettings,
         formatterParams: {
           scrollable: true,
-          urlPrefix: '',
-          idField: 'grocerist_id',
-          nameField: 'name'
+          urlPrefix: "",
+          idField: "grocerist_id",
+          nameField: "name",
         },
-        headerFilterFuncParams: { nameField: 'name' },
+        headerFilterFuncParams: { nameField: "name" },
       },
       {
-        title: '# Groceries',
-        field: 'good_count',
-        bottomCalc: 'sum'
+        title: "# Groceries",
+        field: "good_count",
+        bottomCalc: "sum",
       },
       {
-        title: 'Documents',
-        field: 'documents',
+        title: "Documents",
+        field: "documents",
         ...linkListColumnSettings,
         formatterParams: {
           scrollable: true,
-          urlPrefix: '',
-          idField: 'grocerist_id',
-          nameField: 'name'
+          urlPrefix: "",
+          idField: "grocerist_id",
+          nameField: "name",
         },
       },
       {
-        title: '# Docs',
-        field: 'doc_count',
-        bottomCalc: 'sum'
-      }
-    ]
-  })
-})
+        title: "# Docs",
+        field: "doc_count",
+        bottomCalc: "sum",
+      },
+    ],
+  });
+});
