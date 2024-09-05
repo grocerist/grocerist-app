d2775df289eaa483a108a5f53ac7c49c67812a7f
rausch-supola <michaela.rauschsupola@gmail.com>
2024-09-05 12:02:07 +0200

    linting: line too long

--- json_pyscripts/make_chartsjson.py
+++ json_pyscripts/make_chartsjson.py
@@ -323,7 +323,8 @@ def flatten_list(lst):
     return flat_list
 
 
-# Create list of all categories in the correct order with main category, [sub categories], main category, [sub categories], ... still nested
+# Create list of all categories in the correct order with main category, [sub 
+# categories], main category, [sub categories], ... still nested
 main_sub_category_names = {category: [] for category in main_category_names}
 
 for category in categories_data:
