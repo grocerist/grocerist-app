dab87ad114170eb784559b3de6cee01c6bcef8e3
ogoendiken <ozgurgondiken@gmail.com>
2025-02-14 16:02:19 +0100

    prices_update_OG

--- html/js/prices.js
+++ html/js/prices.js
@@ -1,4 +1,4 @@
-const dataUrl = "json_dumps/prices.json";
+const dataUrl = "json_dumps/price_per_document.json";
 
 const baseColumnDefinitions = [
     {
@@ -24,7 +24,35 @@ const baseColumnDefinitions = [
     },
     {
         title: "Total Value",
-        field: "Total_Value",
+        field: "Total Value",
         headerFilter: "input",
     },
 ];
+
+const columnDefinitions = baseColumnDefinitions.map((column) => ({
+    ...column,
+    minWidth: 200,
+  }));
+
+  d3.json(dataUrl, function (data) {
+    data = Object.values(data).filter((item) => item.good !== ""); 
+  
+    let tableData = data.map((item) => {
+      const enriched = item;
+      return {
+        good: item.good,                 
+        price: item.price,               
+        unit: item.unit,                 
+        amount_of_units: item.amount_of_units,
+        total_value: item["Total Value"], 
+      };
+    });
+  
+    var table = new Tabulator("#prices-table", {
+      ...commonTableConfig,
+      data: tableData,
+      columns: columnDefinitions,
+      initialSort: [{ column: "good", dir: "asc" }],
+    });
+  });
+  
